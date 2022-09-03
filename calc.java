d0ae7e9cd8529976cd9788600a82c52380653c97
Niraj <112692957+Niraj8809@users.noreply.github.com>
2022-09-03 14:39:28 +0530

    Update and rename HelloWorld.java to calc.java

---
+++ calc.java
@@ -0,0 +1,38 @@
+public class calc {
+  public static void main(String[] args) {
+     // initializing variables
+        int num1 = 10, num2 = 20, sum = 0, sub = 0, mult = 0 , div = 0;
+  
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+  
+        // adding num1 and num2
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+        
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+  
+        // subtracting num1 and num2
+        sub = num1 - num2;
+        System.out.println("Subtraction = " + sub);
+        
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+  
+        // Multiplying num1 and num2
+        mult = num1 * num2;
+        System.out.println("Multiplication = " + mult);
+        
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+  
+        // Dividing num1 and num2
+        div = num1 / num2;
+        System.out.println("Division = " + div);
+        
+  }
+}
